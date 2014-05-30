fcf3276e16fcd2ca40598b94385d97f220bd0771
greatbody <wedown@163.com>
2014-05-30 11:54:32 +0800

    add draw ability

---
+++ SunSoftUtil/SimpleDraw.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Drawing
+Public Class SimpleDraw
+    Public Sub DrawPoint(ByVal hwnd As Integer, ByVal x As Integer, ByVal y As Integer, ByVal radium As Integer)
+        DrawPoint(hwnd, x, y, radium, Color.Black)
+    End Sub
+    Public Sub DrawPoint(ByVal hwnd As Integer, ByVal x As Integer, ByVal y As Integer, ByVal radium As Integer,
+                         ByVal color As Color)
+        Dim bm = Graphics.FromHwnd(hwnd)
+        Dim brush = New SolidBrush(color)
+        bm.FillEllipse(brush, x - radium, y - radium, radium, radium)
+        bm = Nothing
+    End Sub
+End Class
