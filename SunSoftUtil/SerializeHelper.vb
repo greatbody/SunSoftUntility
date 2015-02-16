c46041bdff6db23250417eddf6e400e8d2c3f015
greatbody <wedown@163.com>
2015-02-16 19:51:10 +0800

    新增序列化、反序列化类。

---
+++ SunSoftUtil/SerializeHelper.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Runtime.Serialization.Formatters.Binary
+Imports System.IO
+
+Public Class SerializeHelper
+    Public Shared Function DeserializeFromFile(ByVal filePath As String) As Object
+        Using fo As New FileStream(filePath, FileMode.Open)
+            Dim deserizer As New BinaryFormatter
+            Return deserizer.Deserialize(fo)
+        End Using
+    End Function
+
+    Public Shared Sub SerializeObjToFile(ByVal obj As Object, ByVal filePath As String)
+        Using fo As New FileStream(filePath, FileMode.Create)
+            Dim serizer As New BinaryFormatter
+            serizer.Serialize(fo, obj)
+        End Using
+    End Sub
+End Class
